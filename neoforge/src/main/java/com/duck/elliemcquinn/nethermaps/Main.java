34f45c1ce8c33ae75cdd619fb613fa272bb2a82b
Ellie McQuinn <elliemcquinn@duck.com>
2025-03-13 14:41:10 +0000

    Leave some comments for when I pick up this project again.

--- neoforge/src/main/java/com/duck/elliemcquinn/nethermaps/Main.java
+++ neoforge/src/main/java/com/duck/elliemcquinn/nethermaps/Main.java
@@ -8,7 +8,10 @@ import net.neoforged.fml.config.ModConfig;
 @Mod("nethermaps")
 public class Main {
 
+    // todo: Unsure of which sides the mod needs to be loaded on, update config type once known.
     public Main(IEventBus modBus, ModContainer container) {
-        container.registerConfig(ModConfig.Type.CLIENT, Config.SPEC);
+        container.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
+        // todo: Kind of lost interest for the moment, see MapItemSavedData.tickCarriedBy for where to inject, should remove the dimension check and then somehow update the players location on map if the scaling is different.
     }
 }
